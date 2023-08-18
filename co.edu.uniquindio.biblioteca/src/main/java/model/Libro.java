ec3fe02aafc5da1c16565b39bbadc34c7b8e2bef
Oscar Marin <marinmolinao@gmail.com>
2023-08-17 19:36:29 -0500

    Creacion del proyecto y cracion de la clase Libro en el package model

---
+++ co.edu.uniquindio.biblioteca/src/main/java/model/Libro.java
@@ -0,0 +1,58 @@
+package model;
+
+public class Libro {
+    private String nombre;
+    private String genero;
+    private String autor;
+    private int anioPublicacion;
+    private String editorial;
+    private int numeroPaginas;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getAnioPublicacion() {
+        return anioPublicacion;
+    }
+
+    public void setAnioPublicacion(int anioPublicacion) {
+        this.anioPublicacion = anioPublicacion;
+    }
+
+    public String getEditorial() {
+        return editorial;
+    }
+
+    public void setEditorial(String editorial) {
+        this.editorial = editorial;
+    }
+
+    public int getNumeroPaginas() {
+        return numeroPaginas;
+    }
+
+    public void setNumeroPaginas(int numeroPaginas) {
+        this.numeroPaginas = numeroPaginas;
+    }
+}
